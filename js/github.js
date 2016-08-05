80c146cea0fdf4bdba8e8623b5f21440ca094fe6
Maarja Laineste <maarja.laineste@gmail.com>
2016-08-05 11:45:01 -0700

    getRepos prototype displays 30 repos

--- js/github.js
+++ js/github.js
@@ -5,11 +5,21 @@ var UserLookup = function() {
 };
 
 UserLookup.prototype.getRepos = function(username) {
-  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
+    console.log(response.length);
+    for (var i = 0; i < response.length; i++) {
+      $('#results').append('<li> Name: ' + response[i].name + '</li>' +
+                            '<li>' + response[i].description + '</li>');
+    }
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
+
+
+
+
 
 exports.userModule = UserLookup;
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -3,6 +3,7 @@ var UserLookup = require('./../js/github.js').userModule;
 
 $(document).ready(function() {
   $('.user-lookup').submit(function(event) {
+    $('#results').empty();
     event.preventDefault();
     var userSearch = new UserLookup();
     var username = $('#username').val();
